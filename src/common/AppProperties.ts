c6b1401ac86bfd945f2433ae41d757f9e9b574bc
clementgyimah <clementgyimah2@gmail.com>
2022-06-17 16:37:53 +0000

    feat(production): api url changed to production

--- src/common/AppProperties.ts
+++ src/common/AppProperties.ts
@@ -1,4 +1,5 @@
-export const API_URL =
+/*export const API_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5000"
-    : "https://really-great-chat-backend.herokuapp.com";
+    : "https://really-great-chat-backend.herokuapp.com";*/
+export const API_URL = "https://really-great-chat-backend.herokuapp.com";
